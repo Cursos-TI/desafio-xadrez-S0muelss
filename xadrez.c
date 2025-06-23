94646f8a58ec33bb5df834994760bbe7fb46a1f1
S0muelss <samuelalexandre241@gmail.com>
2025-06-23 17:47:08 +0000

    movimentações adicionadas

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,34 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+// movimentação da torre
+for(int mt = 0; mt <= 5 ; mt++){ //logica que simula a movimentação da torre
+    printf("Torre movimentada  a direita\n"); // saida indicando a direção da torre
 }
+ //movimentação da rainha
+ int movimentacaorainha = 0; // variável para gerar a simulação
+  while(movimentacaorainha < 8){ //simulação do movimento
+    printf("Rainha movimentada a esquerda\n"); // indicação do movimento
+    movimentacaorainha ++;//quebra de looping
+}
+// movimentação bispo
+int i, movimentacaoBispo = 0;
+printf("ESCOLHA UMA DIREÇÃO MPARA MOVIMENTAR O BISPO\n"); //escolha das direções de movimentação
+printf("[1]- DIAGONAL DIREITA\n");
+printf("[2]-DIAGONAL ESQUERDA\n");
+scanf("%d", &i);
+
+do{
+if(i== 1){ // loop para simular a movimentação
+    printf("Bispo foi movimentado diagonalmente a direita\n"); 
+}else if (i == 2){ //saida informando a direção
+    printf("Bispo foi movimentado diagonalmente a esquerda\n");
+} else{
+    printf("Opção inválida"); // caso seja uma opção que não atenda a condição
+}
+movimentacaoBispo++; // atualização para evitar o loopin infinito
+}while(movimentacaoBispo < 8);
+ return 0;
+}
+
+
