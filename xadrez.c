97ac8f070cc8c7096a064f19814ebddebd20d9e1
S0muelss <samuelalexandre241@gmail.com>
2025-06-23 18:56:24 +0000

    movimentação do cavalo e melhorias feitas

--- xadrez.c
+++ xadrez.c
@@ -11,28 +11,31 @@ for(int mt = 0; mt <= 5 ; mt++){ //logica que simula a movimentação da torre
 }
  //movimentação da rainha
  int movimentacaorainha = 0; // variável para gerar a simulação
+
   while(movimentacaorainha < 8){ //simulação do movimento
-    printf("Rainha movimentada a esquerda\n"); // indicação do movimento
-    movimentacaorainha ++;//quebra de looping
+     printf("Rainha movimentada a esquerda\n"); // indicação do movimento
+     movimentacaorainha ++;//quebra de looping
 }
 // movimentação bispo
-int i, movimentacaoBispo = 0;
-printf("ESCOLHA UMA DIREÇÃO MPARA MOVIMENTAR O BISPO\n"); //escolha das direções de movimentação
-printf("[1]- DIAGONAL DIREITA\n");
-printf("[2]-DIAGONAL ESQUERDA\n");
-scanf("%d", &i);
+int movimentacaoBispo = 0;
 
 do{
-if(i== 1){ // loop para simular a movimentação
-    printf("Bispo foi movimentado diagonalmente a direita\n"); 
-}else if (i == 2){ //saida informando a direção
-    printf("Bispo foi movimentado diagonalmente a esquerda\n");
-} else{
-    printf("Opção inválida"); // caso seja uma opção que não atenda a condição
-}
-movimentacaoBispo++; // atualização para evitar o loopin infinito
+  printf("bispo movimentado para cima/direita");//descrição do movimento
+  movimentacaoBispo++; // atualização para evitar o loopin infinito
 }while(movimentacaoBispo < 8);
- return 0;
+ 
+
+
+// movimentação do cavalo
+int movimentacaoCavalufinal = 1; // variável para movimento final do cavalo
+
+while(movimentacaoCavalufinal != 0){ // loopin externo para realizar o movimento final
+   for(int i = 0; i < 2 ; i++) { //loop interno para realizar o primeiro movimento
+     printf("Cima\n"); // saída do primeiro movimento
 }
+printf("Direita"); // saída do ultimo movimento
+movimentacaoCavalufinal--; // decremento para finalização do movimento
+}
+return 0;
 
-
+}
